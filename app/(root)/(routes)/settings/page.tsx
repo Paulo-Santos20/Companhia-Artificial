c76c8a0e720338ca71d0703fd39927462673ac0b
Paulo dos Santos <paulo_santos20@outlook.com.br>
2023-10-09 21:35:33 -0300

    feat: settings page

---
+++ app/(root)/(routes)/settings/page.tsx
@@ -0,0 +1,12 @@
+const SettingsPage = async () => {
+
+  return ( 
+    <div className="h-full p-4 space-y-2">
+      <h3 className="text-lg font-medium">Configurações</h3>
+      <div className="text-muted-foreground text-sm">
+      </div>
+    </div>
+   );
+}
+ 
+export default SettingsPage;
